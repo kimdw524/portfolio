bac7e4f770fc19b6af49e0be4ded8d09cf0e0ac6
kimdw524 <kimdw524@naver.com>
2024-11-24 20:10:57 +0900

    chore: Pretendard 폰트 적용 (#2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from 'next';
 
+import localFont from 'next/font/local';
+
+import './globals.css';
+
+const pretendard = localFont({
+  src: '../assets/fonts/PretendardVariable.woff2',
+  display: 'swap',
+});
+
 export const metadata: Metadata = {
   title: '김다운 포트폴리오',
   description: '김다운의 포트폴리오',
@@ -11,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
